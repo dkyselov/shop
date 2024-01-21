76c6b35659d49775e2ce18c4ee0f6bb1a0cdc474
Kyselov Dmytro <d.kyselov@d.mobilunity.com>
2024-01-21 22:53:39 +0200

    add auth + server side

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../shared/services/auth/auth.service';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {Subscription} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+  ],
+  providers: [AuthService],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnDestroy {
+  hide = true;
+  loginForm: FormGroup;
+
+  subscription = new Subscription()
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit(): void {
+    console.log('submit')
+    const email = this.loginForm.get('email')?.value || '';
+    const password = this.loginForm.get('password')?.value || '';
+
+    const login = this.authService.login(email, password)
+      .subscribe(result => {
+        if (result) {
+          this.router.navigate(['/'])
+        } else {
+          alert('Something went wrong')
+        }
+      })
+
+    this.subscription.add(login)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
 
 }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../../interfaces/user';
+import {environment} from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  registerUser(newUser: User): Observable<User> {
+    newUser.id = '';
+    return this.http.post<User>(environment.apiBaseUrl + '/api/register', newUser);
+  }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(environment.apiBaseUrl + '/login', {email, password});
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('authToken');
+    const helper = new JwtHelperService();
+    const isExpired = helper.isTokenExpired(token);
+    return !isExpired;
+  }
 }
